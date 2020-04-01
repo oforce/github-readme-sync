0dbe4d910ca50c3c1d101d1ee40d3c3a3c438793
Michael Doyle <michael.doyle@oforce.com>
2020-04-01 12:36:17 -0700

    Console logs

--- main.js
+++ main.js
@@ -17,7 +17,10 @@ async function run() {
     const client = new github.GitHub(token);
     console.log(`Client: ${client}`);
     console.log(`Repo Repository: ${JSON.stringify(github.context.repository)}`);
-    console.log(`Repo Repo: ${JSON.stringify(github.context.repo)}`);
+    console.log("Repo Repo:" + JSON.stringify(github.context.repo));
+
+    console.log("Ref: " + JSON.stringify(github.context.ref))
+    console.log("PAth: " + apiFilePath)
 
     const apiFile = await client.repos.getContents({
       owner: github.context.repo.owner,
